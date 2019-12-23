9ad5d368801a3673fc6a9b6a8c35e5b93199610e
n01e0 <noleo@vanillabeans.mobi>
2019-12-23 10:07:39 +0900

    陽キャだから陰フルエンザとか相手にならんわ✋

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use std::env;
 use std::fs;
 
+struct BrainFuck<'a> {
+    code: &'a std::str::Chars<'a>,
+    c_index: usize,
+    buf: Vec<u8>,
+    b_index: usize
+}
+
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
@@ -16,83 +24,93 @@ fn main() {
 
 fn fuck(code: String) {
     let _eof: char = 0 as char;
-    let mut buf: [u8; 2048] = [0; 2048];
-    let mut b_index: usize = 0;
-    let code = code.chars();
-    let max = code.clone().count();
-    let mut c_index: usize = 0;
+    let mut fucked = BrainFuck {
+        code: &code.chars(),
+        c_index: 0,
+        buf: Vec::new(),
+        b_index: 0
+    };
+    fucked.buf.push(0);
+    let max = code.chars().count();
+
     loop {
-        if c_index == max {
+        if fucked.c_index == max {
             std::process::exit(0);
         }
-        let cur = code.clone().nth(c_index).unwrap();
+        let cur = fucked.code.clone().nth(fucked.c_index).unwrap();
         match cur {
             '+' => {
-                buf[b_index] += 1;
-                c_index += 1;
+                fucked.buf[fucked.b_index] += 1;
+                fucked.c_index += 1;
             },
             '-' => {
-                buf[b_index] -= 1;
-                c_index += 1;
+                fucked.buf[fucked.b_index] -= 1;
+                fucked.c_index += 1;
             },
             '>' => {
-                b_index += 1;
-                c_index += 1;
+                fucked.buf.push(0);
+                fucked.b_index += 1;
+                fucked.c_index += 1;
             },
             '<' => {
-                b_index -= 1;
-                c_index += 1;
+                fucked.b_index -= 1;
+                fucked.c_index += 1;
             },
             '.' => {
-                print!("{}", buf[b_index] as char);
-                c_index += 1;
+                let c: &u8 = fucked.buf.get(fucked.b_index).unwrap_or(&0);
+                print!("{}", *c as char);
+                fucked.c_index += 1;
             },
             ',' => {
-                getc(&mut buf[b_index]);
-                c_index += 1;
+                getc(&mut fucked.buf[fucked.b_index]);
+                fucked.c_index += 1;
             },
-            '[' => {
-                if buf[b_index] == 0 {
-                    let mut nest: i64 = 0;
-                    let mut _cur: char = 0 as char;
-                    loop {
-                        c_index += 1;
-                        _cur = code.clone().nth(c_index).unwrap();
-                        if _cur == '[' {
-                            nest += 1;
-                        } else if _cur == ']' {
-                            nest -= 1;
-                            if nest < 0 {
-                                break;
-                            }
-                        }
-                    }
-                }
-                c_index += 1;
-            },
-            ']' => {
-                if buf[b_index] != 0 {
-                    let mut nest: i64 = 0;
-                    let mut _cur: char = 0 as char;
-                    loop {
-                        c_index -= 1;
-                        _cur = code.clone().nth(c_index).unwrap();
-                        if _cur == ']' {
-                            nest += 1;
-                        } else if _cur =='[' {
-                            nest -= 1;
-                            if nest < 0 {
-                                break;
-                            }
-                        }
-                    }
-                }
-                c_index += 1;
-            },
+            '[' => bf_loop_start(&mut fucked),
+            ']' => bf_loop_gool(&mut fucked),
             tmp if tmp == _eof => break,
-            _ => c_index += 1,
+            _ => fucked.c_index += 1,
         }
     }
+}
+
+fn bf_loop_start(fucked: &mut BrainFuck) {
+    if fucked.buf[fucked.b_index] == 0 {
+        let mut nest: i64 = 0;
+        let mut _cur: char = 0 as char;
+        loop {
+            fucked.c_index += 1;
+            _cur = fucked.code.clone().nth(fucked.c_index).unwrap();
+            if _cur == '[' {
+                nest += 1;
+            } else if _cur == ']' {
+                nest -= 1;
+                if nest < 0 {
+                    break;
+                }
+            }
+        }
+    }
+    fucked.c_index += 1;
+}
+
+fn bf_loop_gool(fucked: &mut BrainFuck) {
+    if fucked.buf[fucked.b_index] != 0 {
+        let mut nest: i64 = 0;
+        let mut _cur: char = 0 as char;
+        loop {
+            fucked.c_index -= 1;
+            _cur = fucked.code.clone().nth(fucked.c_index).unwrap();
+            if _cur == ']' {
+                nest += 1;
+            } else if _cur =='[' {
+                nest -= 1;
+                if nest < 0 {
+                    break;
+                }
+            }
+        }
+    }
+    fucked.c_index += 1;
 }
 
 fn usage() {
