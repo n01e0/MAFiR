74f5911f1abfedc2cf593abe8d684f762294c6ac
n01e0 <noleo@vanillabeans.mobi>
2019-12-24 23:03:01 +0900

    refactor

--- src/bf.rs
+++ src/bf.rs
@@ -1,4 +1,7 @@
 extern crate libc;
+extern crate colored;
+
+use colored::*;
 
 struct BrainFuck {
     code: String,
@@ -8,25 +11,109 @@ struct BrainFuck {
 }
 
 impl BrainFuck {
-    fn new(code: String) -> BrainFuck {
-        let ret = BrainFuck{
+    fn new(code: String) -> Self {
+        let mut ret = Self {
                 code: code,
                 c_index: 0,
                 buf: Vec::new(),
                 b_index: 0
-        }; 
+        };
+        ret.buf.push(0);
         return ret;
+    }
+
+    fn offset_reached_length(&mut self) -> bool {
+        self.code.chars().count() == self.c_index
     }
 
     fn getcur(&self) -> char {
         self.code.clone().chars().nth(self.c_index).unwrap()   
     }
 
+    fn loop_start(&mut self) {
+        if self.buf[self.b_index] == 0 {
+            let mut nest: i64 = 0;
+            let mut _cur: char = 0 as char;
+            loop {
+                self.c_index += 1;
+                _cur = self.getcur();
+                if _cur == '[' {
+                    nest += 1;
+                } else if _cur == ']' {
+                    nest -= 1;
+                    if nest < 0 {
+                        break;
+                    }
+                }
+            }
+        }
+        self.c_index += 1;
+    }
+
+    fn loop_gool(&mut self) {
+        if self.buf[self.b_index] != 0 {
+            let mut nest: i64 = 0;
+            let mut _cur: char = 0 as char;
+            loop {
+                self.c_index -= 1;
+                _cur = self.getcur();
+                if _cur == ']' {
+                    nest += 1;
+                } else if _cur =='[' {
+                    nest -= 1;
+                    if nest < 0 {
+                        break;
+                    }
+                }
+            }
+        }
+        self.c_index += 1;
+    }
+
+    fn b_inc(&mut self) {
+        self.buf[self.b_index] += 1;
+        self.c_index += 1;
+    }
+
+    fn b_dec(&mut self) {
+        self.buf[self.b_index] -= 1;
+        self.c_index += 1;
+    }
+
+    fn p_inc(&mut self) {
+        if self.buf[self.b_index] == *self.buf.last().unwrap() {
+            self.buf.push(0);
+        }
+        self.b_index += 1;
+        self.c_index += 1;
+
+    }
+
+    fn p_dec(&mut self) {
+        self.b_index -= 1;
+        self.c_index += 1;
+    
+    }
+
+    fn cur_print(&mut self) {
+        let c: &u8 = self.buf.get(self.b_index).unwrap_or(&0);
+        print!("{}", *c as char);
+        self.c_index += 1;
+    }
+
+    fn getchar(&mut self) {
+        unsafe {
+            self.buf[self.b_index] = libc::getchar() as u8;
+        }
+        self.c_index += 1;
+    }
+
     fn dump(&self) {
         println!();
-        println!("-8<-----8<----8<-----");
-        println!("program end");
-        println!("dump buffer");
+        println!("{}", "-8<-----8<----8<-----".cyan());
+        println!("-----program end-----");
+        println!("{}", "let's dump buffer↓ and last pointer's index is red.".red());
+
         print!("+-----+");
         let len = self.buf.len();
         for _ in 0..len {
@@ -36,7 +123,13 @@ impl BrainFuck {
     
         print!("|index|");
         for i in 0..len {
-            print!("0x{:x>04}|", i);
+            let index = format!("0x{:>04x}", i);
+            if i == self.b_index {
+                print!("{}", index.red());
+            } else {
+                print!("{}", index);
+            }
+            print!("|");
         }
         println!();
     
@@ -47,8 +140,8 @@ impl BrainFuck {
         println!();
     
         print!("|value|");
-        for cur in self.buf.clone() {
-            print!("0x{:x>04}|", cur);
+        for c in self.buf.clone() {
+            print!("0x{:>04x}|", c);
         }
         println!();
     
@@ -62,96 +155,24 @@ impl BrainFuck {
 
 pub fn fuck(code: String) {
     let _eof: char = 0 as char;
-    let mut fucked = BrainFuck::new(code.clone());
-    fucked.buf.push(0);
-    let max = code.chars().count();
+    let mut fucked = BrainFuck::new(code);
 
     loop {
-        if fucked.c_index == max {
+        if fucked.offset_reached_length() {
             fucked.dump();
-            std::process::exit(0);
+            break;
         }
-        let cur = fucked.getcur();
-        match cur {
-            '+' => {
-                fucked.buf[fucked.b_index] += 1;
-                fucked.c_index += 1;
-            },
-            '-' => {
-                fucked.buf[fucked.b_index] -= 1;
-                fucked.c_index += 1;
-            },
-            '>' => {
-                if fucked.buf[fucked.b_index] == *fucked.buf.last().unwrap() {
-                    fucked.buf.push(0);
-                }
-                fucked.b_index += 1;
-                fucked.c_index += 1;
-            },
-            '<' => {
-                fucked.b_index -= 1;
-                fucked.c_index += 1;
-            },
-            '.' => {
-                let c: &u8 = fucked.buf.get(fucked.b_index).unwrap_or(&0);
-                print!("{}", *c as char);
-                fucked.c_index += 1;
-            },
-            ',' => {
-                getc(&mut fucked.buf[fucked.b_index]);
-                fucked.c_index += 1;
-            },
-            '[' => bf_loop_start(&mut fucked),
-            ']' => bf_loop_gool(&mut fucked),
+        match fucked.getcur() {
+            '+' => fucked.b_inc(),
+            '-' => fucked.b_dec(),
+            '>' => fucked.p_inc(),
+            '<' => fucked.p_dec(),
+            '.' => fucked.cur_print(),
+            ',' => fucked.getchar(),
+            '[' => fucked.loop_start(),
+            ']' => fucked.loop_gool(),
             tmp if tmp == _eof => break,
             _ => fucked.c_index += 1,
         }
-    }
-}
-
-fn bf_loop_start(fucked: &mut BrainFuck) {
-    if fucked.buf[fucked.b_index] == 0 {
-        let mut nest: i64 = 0;
-        let mut _cur: char = 0 as char;
-        loop {
-            fucked.c_index += 1;
-            _cur = fucked.code.clone().chars().nth(fucked.c_index).unwrap();
-            if _cur == '[' {
-                nest += 1;
-            } else if _cur == ']' {
-                nest -= 1;
-                if nest < 0 {
-                    break;
-                }
-            }
-        }
-    }
-    fucked.c_index += 1;
-}
-
-fn bf_loop_gool(fucked: &mut BrainFuck) {
-    if fucked.buf[fucked.b_index] != 0 {
-        let mut nest: i64 = 0;
-        let mut _cur: char = 0 as char;
-        loop {
-            fucked.c_index -= 1;
-            _cur = fucked.code.clone().chars().nth(fucked.c_index).unwrap();
-            if _cur == ']' {
-                nest += 1;
-            } else if _cur =='[' {
-                nest -= 1;
-                if nest < 0 {
-                    break;
-                }
-            }
-        }
-    }
-    fucked.c_index += 1;
-}
-
-fn getc(buf: &mut u8) {
-    unsafe {
-        let tmp: libc::c_int = libc::getchar();
-        *buf = tmp as u8;
     }
 }
